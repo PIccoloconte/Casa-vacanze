71b6a44c3f5c2e4bb7ba7cc65fcdc99030be5012
marco cencig <74985021+PIccoloconte@users.noreply.github.com>
2024-02-10 15:33:12 +0100

    10/2/2024
    Aggiunto lo script per il funzionamento del cambio immagine automatico nella home

--- Sito Nonno/main_Home.js
+++ Sito Nonno/main_Home.js
@@ -1,37 +1,101 @@
-const imgCont = document.querySelectorAll(".imgCont__img");
+const imgCont = document.querySelector(".imgCont");
+const imgArray = document.querySelectorAll(".imgCont__img");
 const rightBtn = document.querySelector(".img_Cont__btn--right");
 const leftBtn = document.querySelector(".img_Cont__btn--left");
+const controlBulletCont = document.querySelector(".controlBulletCont");
+
+/*control Bullet div */
+for(let i = 0; i < imgArray.length ; i++){
+    const newControlBullet = document.createElement("div");
+    newControlBullet.classList.add("controlBulletCont__box");
+    newControlBullet.innerHTML = `<div class="controlBulletCont__box__sphere"></div>`
+    controlBulletCont.appendChild(newControlBullet);
+}
+const controlBulletBoxes = document.querySelectorAll(".controlBulletCont__box");
+controlBulletBoxes.forEach((box,idx) =>{
+    box.firstChild.addEventListener("click",(e) =>{
+        SetActiveImg(idx);
+    })
+})
 
 // Immagini
 let activeImg = 0;
+let imgChangeTime = 4000;
+controlBulletBoxes[activeImg].firstChild.classList.add("active");
 
 rightBtn.addEventListener("click",() =>{
-    
     activeImg++;
-
-    if(activeImg > imgCont.length - 1){
-        activeImg = 0;
-    }
-    SetActiveImg();
-
+    CheckImgIndex();
+    resetInterval();
 })
+
 leftBtn.addEventListener("click",()=>{
     activeImg--;
-
-    if(activeImg < 0 ){
-        activeImg = imgCont.length -1;
-    }
-    SetActiveImg();
+    CheckImgIndex();
+    resetInterval();
 })
 
+//Cabio immagine nel tempo
+let interval = setInterval(run,imgChangeTime)
 
-function SetActiveImg(){
-    imgCont.forEach((img) => img.classList.remove("active"));
-
-    imgCont[activeImg].classList.add("active");
+function run() {
+    activeImg++;
+    CheckImgIndex()
 }
 
+function CheckImgIndex(){
+    if(activeImg > imgArray.length - 1){
+        activeImg = 0;
+    }
+    if(activeImg < 0 ){
+        activeImg = imgArray.length -1;
+    }
+    
+    SetActiveImg();
+}
 
-  
-   
+function SetActiveImg(index = null){
+    resetInterval();
+    if(index === null)
+    {
+        activeImg = activeImg;
+    }
+    else{
+        activeImg = index
+    }
+        imgArray.forEach((img) => img.classList.remove("active"));
+        imgArray[activeImg].classList.add("active");
+    
+        controlBulletBoxes.forEach((box) => box.firstChild.classList.remove("active"));
+        controlBulletBoxes[activeImg].firstChild.classList.add("active");
+}
 
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, imgChangeTime)
+}
+
+//Cambio immagine touch per mobile
+  let startingX;
+  let movingX;
+
+imgCont.addEventListener("touchstart", e =>{
+    startingX = e.touches[0].clientX;
+})
+
+imgCont.addEventListener("touchmove",e =>{
+    movingX = e.touches[0].clientX;
+})
+
+imgCont.addEventListener("touchend",e =>{
+    if(startingX + 100 < movingX){
+        activeImg--;
+        CheckImgIndex();
+        resetInterval();
+    }
+    else if(startingX -100 > movingX){
+        activeImg++;
+        CheckImgIndex();
+        resetInterval();
+    }
+})
